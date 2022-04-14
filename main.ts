f62b401433dd86a9c8e54e5ade17d6a031651226
ANTHONY20088 <103639067+ANTHONY20088@users.noreply.github.com>
2022-04-14 12:30:23 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,36 @@
+let n = 0
+while (!(input.buttonIsPressed(Button.B))) {
+    if (input.buttonIsPressed(Button.A)) {
+        n += 1
+        if (n > 3) {
+            n = 0
+        }
+        if (n == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            if (n == 2) {
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showIcon(IconNames.Asleep)
+            }
+        }
+    }
+}
+basic.showString("votre humeur")
+basic.showNumber(n)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
